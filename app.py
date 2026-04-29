aaad1bb6ac8fb1783a55795ed4ef3c5297553b9e
MoARABY <arabym702@gmail.com>
2026-04-29 20:42:31 +0300

    backend : puplish database connection with sqllite

---
+++ app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+from datetime import datetime
+
+DB_NAME = "app.db"
+
+
+def connect():
+    return sqlite3.connect(DB_NAME)
+
+
+# ================= SETUP =================
+def setup_db():
+    conn = connect()
+    cur = conn.cursor()
+
